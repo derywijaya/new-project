7d20f284749c66e66fb73a215579285d3dbd6d13
derywijaya <derywiftui@gmail.com>
2022-10-22 00:05:02 +0700

    tambah

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.markdown('<div style="text-align: center;">Jumlah Timbulan Sampah Indonesia T
 st.dataframe(df)
 dt = pd.read_csv ("Sampah123.csv")
 
-st.markdown('<div style="text-align: center;">Jumlah Timbulan Sampah di Indonesia Perprovinsi Tahun 2021</div>', unsafe_allow_html=True)
+st.markdown('<div style="text-align: center;">Jumlah Timbulan ada Sampah di Indonesia Perprovinsi Tahun 2021</div>', unsafe_allow_html=True)
 
 st.bar_chart(dt)
 
